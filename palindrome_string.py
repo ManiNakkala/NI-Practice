aa9a62aae643fe7f05f1bf891be2a9afe2be4a6c
Mani Sankar <127809137+ManiNakkala@users.noreply.github.com>
2025-04-11 20:36:07 +0530

    new

--- palindrome_string.py
+++ palindrome_string.py
@@ -4,8 +4,10 @@ for _ in s:
     length += 1
 is_pal = True
 for i in range(length//2):
-    is_pal = False
-    break
+    if s[i] != s[length-i-1]:
+        is_pal = False
+        break
+    
 if(pal):
     print("Palindrome")
 else:
